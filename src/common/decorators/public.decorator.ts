58630be504314a889592e3cbe081597d72576556
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-09-24 12:26:48 +0600

    feat: add JWT authentication support with public route handling

--- src/common/decorators/public.decorator.ts
+++ src/common/decorators/public.decorator.ts
@@ -1,5 +1,5 @@
-import { SetMetadata } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+export const Public = Reflector.createDecorator<boolean>({
+  transform: () => true,
+});
---
+++ src/common/guards/jwt-auth.guard.ts
@@ -0,0 +1,24 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Public } from '@common/decorators/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(ctx: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride(Public, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(ctx);
+  }
+}
